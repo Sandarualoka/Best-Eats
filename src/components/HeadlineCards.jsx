3785c2afc7ff2ff2562bf74b9de26730c7beb857
Sandaru  De Silva <sandarualoka99@gmail.com>
2024-01-02 14:30:29 +0530

    adding cards for restuarents

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -3,14 +3,20 @@ import Test1 from "../assets/test1.jpg";
 
 const HeadlineCards = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <p>Test1 Restuarent</p>
-          <p>8/26</p>
-          <button>Order Now</button>
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      <div className="rounded-xl relative">
+        <div className="absolute w-full h-full bg-black/50 text-white rounded-xl">
+          <p className="font-bold text-2xl pt-4 px-2">Test1 Restuarent</p>
+          <p className="px-2">8/26</p>
+          <button className="bg-white text-black border-white mx-2 absolute mt-5">
+            Order Now
+          </button>
         </div>
-        <img src={Test1} alt="test1" />
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src={Test1}
+          alt="test1"
+        />
       </div>
     </div>
   );
